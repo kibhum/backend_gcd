492cce8043a6fda6bea4612b37a6d3efe2471091
kibethh <kibethh@gmail.com>
2025-01-03 16:28:43 +0300

    Added an HTTP server with post gcd

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,13 @@
+use actix_web::{web, App, HttpResponse, HttpServer};
+use serde::Deserialize;
 use std::time;
 use tokio;
+
+#[derive(Deserialize)]
+struct GCDParameters {
+    n: u64,
+    m: u64,
+}
 
 async fn print_odd() {
     let total = 10;
@@ -34,7 +42,22 @@ where
     f("hello world");
 }
 
-#[tokio::main]
+fn gcd(mut n: u64, mut m: u64) -> u64 {
+    assert!(n != 0 && m != 0);
+    while m != 0 {
+        // Exchange the values of m, n if m<n
+        if m < n {
+            let temp = m;
+            m = n;
+            n = temp;
+        }
+        m %= n
+    }
+    n
+}
+
+// #[tokio::main]
+#[actix_web::main]
 async fn main() {
     let odd = print_odd();
     let even = print_even();
@@ -47,4 +70,47 @@ async fn main() {
         println!("{s} {x}");
     };
     func_with_closure(print_x_closure);
+
+    // RUST HTTP SERVER
+    let server = HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(get_index))
+            .route("/gcd", web::post().to(post_gcd))
+    });
+    println!("Server is running port 3000");
+    server
+        .bind("127.0.0.1:3000")
+        .expect("Error binding to the port")
+        .run()
+        .await
+        .expect("Error running the server");
+}
+
+async fn get_index() -> HttpResponse {
+    HttpResponse::Ok().content_type("text/html").body(
+        r#"
+        <title> GCD Calculator</title>
+        <form action="/gcd" method="post">
+        <input type="text" name="n"/>
+        <input type="text" name="m"/>
+        <button type="submit">Compute GCD</button>
+        </form>
+        "#,
+    )
+}
+
+async fn post_gcd(form: web::Form<GCDParameters>) -> HttpResponse {
+    if form.m == 0 || form.n == 0 {
+        return HttpResponse::BadRequest()
+            .content_type("text/html")
+            .body("Computing the GCD with ZERO is boring");
+    }
+
+    let response = format!(
+        "The greatest common divisor of {} and {} is <b>{}</b>",
+        form.m,
+        form.n,
+        gcd(form.m, form.n)
+    );
+    HttpResponse::Ok().content_type("text/html").body(response)
 }
